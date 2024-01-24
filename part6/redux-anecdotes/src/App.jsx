5a676a7f8a221de2c8c05f491e117d448ccb4ed7
Thomas ROBERT <thomas_3004@hotmail.fr>
2024-01-24 09:56:51 +0100

    Add anecdotes filter

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -2,9 +2,16 @@ import { useSelector, useDispatch } from 'react-redux';
 import { createAnecdote, voteForAnecdote } from './reducers/anecdoteReducer.js';
 import AnecdoteForm from './components/AnecdoteForm.jsx';
 import AnecdoteList from './components/AnecdoteList.jsx';
+import Filter from './components/Filter.jsx';
 
 const App = () => {
-  const anecdotes = useSelector((state) => state);
+  const anecdotes = useSelector((state) =>
+    state.anecdotes.filter((anecdote) => {
+      return anecdote.content
+        .toLowerCase()
+        .includes(state.filter.toLowerCase());
+    })
+  );
   const dispatch = useDispatch();
   const sortedAnecdotes = anecdotes.toSorted((a, b) => b.votes - a.votes);
 
@@ -19,6 +26,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <AnecdoteList anecdotes={sortedAnecdotes} />
       <AnecdoteForm onSubmit={addAnecdote} vote={vote} />
     </div>
